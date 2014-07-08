a84b287db28ab02fa0a2aa285992410e1ac44149
Riduidel <nicolas.delsaux@gmail.com>
2014-07-08 14:56:09 +0200

    J'ajoute un contrôleur différent histoire de montrer que tout ne doit pas être dans le même contrôleur, parce que wisdom est trop fort.

---
+++ src/main/java/org/ndx/wisdom/tutorial/angular/GoogleShopController.java
@@ -0,0 +1,13 @@
+package org.ndx.wisdom.tutorial.angular;
+
+import org.wisdom.api.DefaultController;
+import org.wisdom.api.annotations.Controller;
+import org.wisdom.api.annotations.Route;
+import org.wisdom.api.annotations.View;
+import org.wisdom.api.http.HttpMethod;
+import org.wisdom.api.http.Result;
+import org.wisdom.api.templates.Template;
+
+@Controller
+public class GoogleShopController  extends DefaultController {
+}
