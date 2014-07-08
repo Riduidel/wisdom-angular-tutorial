25aeed3a53764244a497ff505f1b03f30e55209d
Riduidel <nicolas.delsaux@gmail.com>
2014-07-08 15:08:44 +0200

    Ce qu'il faut pour avoir "fait" l'étape https://docs.angularjs.org/tutorial/step_01 du tutoriel angular.js

--- src/main/java/org/ndx/wisdom/tutorial/angular/GoogleShopController.java
+++ src/main/java/org/ndx/wisdom/tutorial/angular/GoogleShopController.java
@@ -9,5 +9,21 @@ import org.wisdom.api.http.Result;
 import org.wisdom.api.templates.Template;
 
 @Controller
-public class GoogleShopController  extends DefaultController {
+public class GoogleShopController extends DefaultController {
+	/**
+	 * Ici LIST est le nom du fichier du template, sans l'extension thl.html
+	 */
+	@View("list")
+	Template list;
+
+	/**
+	 * Et donc quand on va aller sur http://localhost:9000/list, ça va afficher
+	 * le template {@link #list}
+	 * 
+	 * @return
+	 */
+	@Route(method = HttpMethod.GET, uri = "/list")
+	public Result todoList() {
+		return ok(render(list));
+	}
 }
