7b968a6a9e4f2c1caeccda337bd5c71ff5c9554f
Riduidel <nicolas.delsaux@gmail.com>
2014-07-09 14:09:26 +0200

    Enfin une version du test qui marche

--- src/test/java/org/ndx/wisdom/tutorial/angular/Step03IsImplementedIT.java
+++ src/test/java/org/ndx/wisdom/tutorial/angular/Step03IsImplementedIT.java
@@ -1,45 +1,40 @@
 package org.ndx.wisdom.tutorial.angular;
 
-import java.util.concurrent.TimeUnit;
 
-import junit.framework.Assert;
-
+import org.fluentlenium.assertj.FluentLeniumAssertions;
 import org.fluentlenium.core.domain.FluentList;
 import org.fluentlenium.core.domain.FluentWebElement;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.wisdom.test.parents.WisdomFluentLeniumTest;
 
-import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Step03IsImplementedIT  extends WisdomFluentLeniumTest {
+	@Before
+	public void makeSureWeUseFirefox() {
+        assertThat(System.getProperty("fluentlenium.browser")).isEqualTo("firefox");
+	}
 
+	/**
+	 * @see http://stackoverflow.com/q/24648963/15619 pour les détails sur le test du driver
+	 */
 	@Test
 	public void canTestPageCorrectly() {
-		if (getDriver() instanceof HtmlUnitDriver) {
-			HtmlUnitDriver driver = (HtmlUnitDriver) getDriver();
-			if(!driver.isJavascriptEnabled()) {
-				driver.setJavascriptEnabled(true);
-			}
-			Assert.assertTrue("Javascript should be enabled for Angular to work !", driver.isJavascriptEnabled());
-		}
+		assertThat(getDriver()).isInstanceOf(FirefoxDriver.class);
 		goTo(GoogleShopController.LIST);
-		await().untilPage();
-		// Et on charge la liste des téléphones
-		FluentWebElement phones = findFirst(".phones");
-		assertThat(phones).isDisplayed();
-		// pure black magic script that should ensure angular has totally loaded data
-		FluentList<FluentWebElement> items = find(".phone");
-		assertThat(items).hasSize(3);
+		FluentList<FluentWebElement> items = find("li");
+		FluentLeniumAssertions.assertThat(items).hasSize(3);
 		fill("input").with("nexus");
 		await();
 		items = find(".phone");
-		assertThat(items).hasSize(1);
+		FluentLeniumAssertions.assertThat(items).hasSize(1);
 		fill("input").with("motorola");
 		await();
 		items = find(".phone");
-		assertThat(items).hasSize(2);
+		FluentLeniumAssertions.assertThat(items).hasSize(2);
 	}
 
 }
